abbb0cc758c03bfe18397964a61ad908cbc1518c
Breyten Ernsting <bje@dds.nl>
2017-11-22 18:47:15 +0100

    Whhops, make sure we do not do a full archival reindexing everytime we update. See #5

--- bin/update.sh
+++ bin/update.sh
@@ -2,4 +2,4 @@
 
 source /opt/bin/activate
 cd /opt/pfl
-./manage.py extract list_sources | awk '{print $2}' | sed 1d | xargs -I{} ./manage.py extract start {}
+./manage.py extract list_sources | grep -v '_archives_ '| awk '{print $2}' | sed 1d | xargs -I{} ./manage.py extract start {}

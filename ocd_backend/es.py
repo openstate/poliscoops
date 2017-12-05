802b7dadb1249725b29c2260d0e5892bb5ab9d91
Sicco van Sas <siccovansas@users.noreply.github.com>
2017-12-05 18:33:25 +0100

    Increase Elasticsearch query timeout from the default 10 seconds to 20 seconds as we sometimes hit the 10 second timeout

--- ocd_backend/es.py
+++ ocd_backend/es.py
@@ -4,7 +4,7 @@ from ocd_backend.settings import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT
 
 
 def setup_elasticsearch(host=ELASTICSEARCH_HOST, port=ELASTICSEARCH_PORT):
-    return Elasticsearch([{'host': host, 'port': port}])
+    return Elasticsearch([{'host': host, 'port': port, 'timeout': 20}])
 
 
 elasticsearch = setup_elasticsearch()

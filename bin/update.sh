fb616529cf0d0c90a5eef6e150c45219d86d1ff4
Breyten Ernsting <bje@dds.nl>
2018-08-06 10:33:27 +0200

    Split facebook and non-facebook even more evenly.

--- bin/update.sh
+++ bin/update.sh
@@ -8,14 +8,40 @@ if [ ! -e .updating ];
 then
   echo "Creating updating files"
   touch .updating
-  ./manage.py extract list_sources 2>/dev/null | grep -v '_archives_' | awk '{print $2}' | sed 1d >.updating-sources
+
+  # split for non FB sources
+  ./manage.py extract list_sources 2>/dev/null | grep -v '_archives_' | |grep -v '_fb_' |awk '{print $2}' | sed 1d >.updating-sources
   PFL_NUM_SOURCES=`cat .updating-sources |wc -l`
   # Aim to crawl everything every 6 hours. At 4 times per hour this is 24 cycles
   PFL_BLOCK_SIZE=`expr $PFL_NUM_SOURCES / 24 + 1`
   split -l $PFL_BLOCK_SIZE .updating-sources .updating-sources-
+
+  # split for FB sources
+  ./manage.py extract list_sources 2>/dev/null | grep -v '_archives_' | |grep '_fb_' |awk '{print $2}' | sed 1d >.updating-facebook-sources
+  PFL_NUM_SOURCES=`cat .updating-facebook-sources |wc -l`
+  # Aim to crawl everything every 6 hours. At 4 times per hour this is 24 cycles
+  PFL_BLOCK_SIZE=`expr $PFL_NUM_SOURCES / 24 + 1`
+  split -l $PFL_BLOCK_SIZE .updating-facebook-sources .updating-facebook-sources-
 fi
 
+
+# update non FB sources
 PFL_CURRENT_FILE=`ls -1 .updating-sources-* 2>/dev/null |head -1`
+
+if [ -n "$PFL_CURRENT_FILE" ];
+then
+  PFL_BUSY_FILE=`echo -n "$PFL_CURRENT_FILE" |sed -e 's/sources/busy/;'`
+  mv "$PFL_CURRENT_FILE" "$PFL_BUSY_FILE"
+  # Update all sources but not their archives (i.e., only the most recent articles)
+  echo "Starting extraction of $PFL_BUSY_FILE"
+  cat $PFL_BUSY_FILE | xargs -I{} ./manage.py extract start {}
+  echo "Removing $PFL_BUSY_FILE"
+  rm -f $PFL_BUSY_FILE
+  echo "Removed $PFL_BUSY_FILE"
+fi
+
+# update FB sources
+PFL_CURRENT_FILE=`ls -1 .updating-facebook-sources-* 2>/dev/null |head -1`
 
 if [ -n "$PFL_CURRENT_FILE" ];
 then

3960b1ed895acf44e4ac068a6224fa91c580d328
Breyten Ernsting <bje@dds.nl>
2019-01-14 15:55:35 +0100

    Covert locations of local sources.

--- bin/make_lokaal_sources.py
+++ bin/make_lokaal_sources.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import json
+import csv
 
 sys.path.insert(0, '.')
 from ocd_backend.utils.misc import slugify
 
 
-def convert_party(party):
+def convert_party(party, locations):
     slug = slugify(party['Partij']).replace('-', '_')
     slug_location = slugify(party['RegioNaam']).replace('-', '_')
     result = {
@@ -30,7 +32,7 @@ def convert_party(party):
         "loader": "ocd_backend.loaders.ElasticsearchLoader",
         "item": "ocd_backend.items.feed.FeedPhantomJSItem",
         "cleanup": "ocd_backend.tasks.CleanupElasticsearch",
-        "location": party['RegioNaam'],
+        "location": _normalize_location(party['RegioNaam'], locations),
         "hidden": False,
         "id": "%s_%s_1" % (slug, slug_location,)
     }
@@ -38,7 +40,28 @@ def convert_party(party):
     return result
 
 
+def _get_normalized_locations():
+    loc_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        '/opt/pfl/ocd_backend/data/cbs-name2018-mapping.csv')
+    result = {}
+    with open(loc_path) as locations_in:
+        locations = reader = csv.reader(locations_in)
+        headers = locations.next()
+        for location in locations:
+            record = dict(zip(headers, location))
+            result[record[u'Key_poliflw']] = record[u'Alt_map']
+    return result
+
+
+def _normalize_location(location, LOCATIONS):
+    if unicode(location) in LOCATIONS:
+        return LOCATIONS[unicode(location)]
+    return unicode(location)
+
+
 def main(argv):
+    locations = _get_normalized_locations()
     parties = []
 
     result = []
@@ -47,7 +70,7 @@ def main(argv):
 
     for party in parties:
         if party['Feed'] != '':
-            result.append(convert_party(party))
+            result.append(convert_party(party, locations))
 
     print json.dumps(result, indent=2)
     return 0

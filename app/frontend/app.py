903afe42a1155110df4eabb8b7219d4cba33fb76
Breyten Ernsting <bje@dds.nl>
2019-01-18 14:52:57 +0100

    Fix party logo

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -76,8 +76,11 @@ def image_rewrite(url, doc_id):
 
 @app.template_filter('party_image')
 def do_party_image(s):
-    if os.path.exists('/opt/app/frontend/static/images/parties/%s.png' % (s,)):
-        return s
+    if os.path.exists(
+        '/opt/app/frontend/static/images/parties/%s.png' % (
+            s.replace('/', '_'),)
+    ):
+            return s
     else:
         return 'backdrop'
 

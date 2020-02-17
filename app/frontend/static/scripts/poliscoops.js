1b6f6c587d002e487cc38e9030d90c85c93f4088
Breyten Ernsting <bje@dds.nl>
2020-02-17 16:01:46 +0100

    Import CSS into correct template.

--- app/frontend/static/scripts/poliscoops.js
+++ app/frontend/static/scripts/poliscoops.js
@@ -7,3 +7,5 @@ import 'bootstrap-table/dist/locale/bootstrap-table-nl-NL.min.js';
 import 'bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-header.min.js';
 import 'bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js';
 import naturalSort from 'javascript-natural-sort';
+
+console.log('poliscoops inited correctly!');

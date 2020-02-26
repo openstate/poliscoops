b785ce4b7a73678217e2f7409c8f9340db2a152e
Breyten Ernsting <bje@dds.nl>
2020-02-26 17:48:17 +0100

    Country select page sort of working.

--- app/frontend/static/scripts/poliscoops.js
+++ app/frontend/static/scripts/poliscoops.js
@@ -8,4 +8,15 @@ import 'bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-hea
 import 'bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js';
 import naturalSort from 'javascript-natural-sort';
 
-console.log('poliscoops inited correctly!');
+$(function() {
+  console.log('poliscoops inited correctly!');
+
+  $('input[type="checkbox"]').on('change', function() {
+    var state = $('#' + $(this).attr('id')).is(':checked');
+    if (state) {
+      $('label[for="'+ $(this).attr('id')+'"] i').removeClass('fa-square-o').addClass('fa-check-square-o');
+    } else {
+      $('label[for="'+ $(this).attr('id')+'"] i').removeClass('fa-check-square-o').addClass('fa-square-o');
+    }
+  });
+});
